5f11a5f9a05982eb1b798ee0968bab4a6f60b19f
riteshsingla36 <ritesh.singla36@gmail.com>
2022-09-07 00:23:49 +0530

    address router issues resolved

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const app = express();
 const categoryRouter = require("./routes/category.route");
 const subCategoryRouter = require("./routes/sub_category.route");
 const userRouter = require("./routes/user.route");
+const addressRouter = require("./routes/address.route");
 
 dotenv.config({path: "./.env"});
 mongoose.connect(process.env.DB_URI).then(() => console.log("moongoose connected successfully")).catch(error => console.log(error.meaasge));
@@ -17,6 +18,7 @@ app.use(cors());
 app.use("/category", categoryRouter);
 app.use("/subcategory", subCategoryRouter);
 app.use("/user", userRouter);
+app.use("/address", addressRouter);
 
 app.listen(8000, console.log("Your app is running in 8000 port"))
 
