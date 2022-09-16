6b55049bddb1f376563544bee27e5bc66bbc6a5a
deepu024 <ojashsharma024@gmail.com>
2022-09-16 10:39:55 +0530

    add user authentication

--- server/server.js
+++ server/server.js
@@ -12,18 +12,32 @@ const productRouter = require("./routes/product.route");
 const cartRouter = require("./routes/cart.route");
 const cartItemRouter = require("./routes/cart_item.route");
 const cloudinary = require('cloudinary').v2;
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const onBoarding = require('./routes/onboarding.route');
 
 dotenv.config({ path: "./.env" });
 mongoose.connect(process.env.DB_URI).then(() => console.log("moongoose connected successfully")).catch(error => console.log(error.meaasge));
 app.use(express.json());
 app.use(cors());
-
+app.use(cookieParser());
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true
 });
+
+app.use(session({  
+  name: 'EasyShop',
+  secret: process.env.SESSION_SECRET,  
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: false, // This will only work if you have https enabled!
+    maxAge: 60000 // 1 min
+  } 
+}));
 
 app.use("/category", categoryRouter);
 app.use("/subcategory", subCategoryRouter);
@@ -32,6 +46,7 @@ app.use("/address", addressRouter);
 app.use("/product", productRouter);
 app.use("/cart", cartRouter);
 app.use("/cartitem", cartItemRouter);
+app.use('/auth', onBoarding);
 
 app.listen(8000, console.log("Your app is running in 8000 port"));
 
